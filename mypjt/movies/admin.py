f3db69f21c4414d9efa9f14f1561edf16fd12e52
seulgi <0_1998@naver.com>
2023-04-07 16:02:04 +0900

    김슬기첫번째커밋

--- mypjt/movies/admin.py
+++ mypjt/movies/admin.py
@@ -1,3 +1,7 @@
+# admin.py
 from django.contrib import admin
 
+from .models import Movie
+
 # Register your models here.
+admin.site.register(Movie)
--- mypjt/movies/views.py
+++ mypjt/movies/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.http import (require_http_methods, require_POST,
                                           require_safe)
 
 from .models import Movie
+from .forms import MovieForm
 
 # Create your views here.
 
@@ -14,3 +15,52 @@ def index(req):
         'movies': movies
     }
     return render(req, 'movies/index.html', context)
+
+
+@require_http_methods(['GET', 'POST'])
+def create(req):
+    if req.method == 'POST':
+        # 파일 및 이미지는 req.FILES에 담기므로 이것도 넘겨줘야함
+        form = MovieForm(req.POST, req.FILES)
+        if form.is_valid():
+            movie = form.save()
+            return redirect('movies:detail', movie.pk)
+    else:
+        form = MovieForm()
+
+    context = {'form': form}
+    return render(req, 'movies/create.html', context)
+
+
+@require_safe
+def detail(req, pk):
+    movie = Movie.objects.get(pk=pk)
+    context = {
+        'movie': movie
+    }
+    return render(req, 'movies/detail.html', context)
+
+
+@require_http_methods(['GET', 'POST'])
+def update(req, pk):
+    movie = Movie.objects.get(pk=pk)
+    if req.method == 'POST':
+        form = MovieForm(req.POST, instance=movie)
+        if form.is_valid():
+            movie = form.save()
+            return redirect('movies:detail', movie.pk)
+    else:
+        form = MovieForm(instance=movie)
+    context = {
+        'form': form,
+        'movie': movie
+    }
+
+    return render(req, 'movies/update.html', context)
+
+
+@require_POST
+def delete(req, pk):
+    movie = Movie.objects.get(pk=pk)
+    movie.delete()
+    return redirect('movies:index')
